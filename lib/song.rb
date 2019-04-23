2e1af840a904bd16d48235c7a5c87737e4202bd4
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-23 19:45:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,55 @@
 class Song
-  attr_accessor :name
-  attr_reader :artist
+  attr_accessor :name, :artist, :genre
 
-  @@songs = []
+   @@count = 0
+  @@genres = []
+  @@artists = []
 
-  def artist=(artist)
-    @artist = artist
-  end
+   def initialize name, artist, genre
+     @name = name
+     @artist = artist
+     @genre = genre
+     @@count += 1
+     @@genres << genre
+     @@artists << artist
+   end
 
-  def self.all
-    @@songs
-  end
-end
+    def self.count
+     @@count
+   end
+
+    def self.genres
+     @@genres.uniq
+   end
+
+    def self.artists
+     @@artists.uniq
+   end
+
+    #Song.genre_count
+     # => {"rap" => 5, "rock" => 1, "country" => 3}
+   def self.genre_count
+     num_of_genres = {}
+     @@genres.each do |each_genre|
+       if num_of_genres[each_genre]
+          num_of_genres[each_genre] += 1 
+        else
+          num_of_genres[each_genre] = 1
+       end
+     end
+     num_of_genres
+   end
+
+    def self.artist_count
+     num_of_artists = {}
+     @@artists.each do |each_artist|
+       if num_of_artists[each_artist]
+         num_of_artists[each_artist] += 1
+       else
+         num_of_artists[each_artist] = 1
+       end
+     end
+     num_of_artists
+   end
+
+ end
